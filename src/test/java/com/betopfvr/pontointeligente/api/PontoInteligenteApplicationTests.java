7245fa87de1ce97d9e35dbd4de7658d334fb4f23
Roberto Dias <robertordias162@gmail.com>
2020-06-23 18:52:12 -0300

    Commit inicial dos arquivos do projeto

---
+++ src/test/java/com/betopfvr/pontointeligente/api/PontoInteligenteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.betopfvr.pontointeligente.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PontoInteligenteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
